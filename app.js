4710a308cb66467577b06a3eb95c8c15c35e0082
Victor <victor.rinnegard@hotmail.com>
2020-10-18 02:06:40 +0100

    Added new user emit

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ io.origins(['https://me-jsramverk.rinnegard.me:443']);
 
 io.on('connection', function (socket) {
     console.info("User connected");
-    
+
+    io.emit('new user', {
+        username: "Server Message",
+        message: "New user connected",
+        time: Date.now()
+    });
+
     socket.on('chat message', function (message) {
         io.emit('chat message', message);
     });
