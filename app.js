8c0ba5a6c1cf3dc42940795a0d814fb5454fa3cb
Victor <victor.rinnegard@hotmail.com>
2020-10-18 02:01:27 +0100

    Fixing origin?

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-io.origins(['https://me-jsramverk.rinnegard.me/']);
+io.origins(['https://me-jsramverk.rinnegard.me:443']);
 
 io.on('connection', function (socket) {
+    console.info("User connected");
+    
     socket.on('chat message', function (message) {
         io.emit('chat message', message);
     });
