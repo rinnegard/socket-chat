3d981b4e3e02d6cdd27b98ffeb86ca6ffc3312eb
Victor <victor.rinnegard@hotmail.com>
2020-10-23 02:14:29 +0100

    Added mongo

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const app = express();
 
+const mongo = require('mongodb').MongoClient;
+
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 io.origins(['https://me-jsramverk.rinnegard.me:443']);
 
-io.on('connection', function (socket) {
-    console.info("User connected");
+mongo.connect('mongodb://localhost:27017/chat', function(error, db){
+    if(error){
+        throw error;
+    }
+    console.log("Mongo Connected");
+    io.on('connection', function (socket) {
+        console.info("Socket connected");
+        let messages = db.collection('messages');
 
-    socket.on('new user', function (message) {
-        io.emit('new user', message);
+        messages.find().toArray(function(error, res){
+            if(error){
+                throw err;
+            }
+
+            io.emit('messages', res);
+        });
+
+        socket.on('new user', function (message) {
+            io.emit('new user', message);
+        });
+
+        socket.on('chat message', function (message) {
+            chat.insert({message}, function(){
+                io.emit('chat message', message);
+            });
+
+        });
     });
 
-    socket.on('chat message', function (message) {
-        io.emit('chat message', message);
-    });
 });
 
 server.listen(3000);
